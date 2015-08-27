87ddd5b36584ca0b1c5ce8f414e34c94d94dbe9c
Bernerd Schaefer <bj.schaefer@gmail.com>
2015-08-26 21:14:31 -0700

    More ANF to Transform module

---
+++ Transform.hs
@@ -0,0 +1,60 @@
+module Transform where
+import Language
+
+--
+-- Conversion to A-Normal Form
+--
+
+coreProgramToANF :: CoreProgram -> CoreProgram
+coreProgramToANF program
+  = map coreScDefnToANF program
+
+coreScDefnToANF (name, args, body)
+  = (name, args, body')
+    where body' = coreExprToANF body
+
+coreExprToANF :: CoreExpr -> CoreExpr
+coreExprToANF expr
+  = anfExpr
+    where
+      maybeLet e []    = e
+      maybeLet e defns = ELet nonRecursive defns e
+
+      (expr', defns) = coreExprToANF' expr []
+
+      anfExpr = maybeLet expr' defns
+
+coreExprToANF' :: CoreExpr -> [CoreDefn] -> (CoreExpr, [CoreDefn])
+
+coreExprToANF' expr@(EAp f a) defns
+  = coreApToANF f a defns
+
+coreExprToANF' e defns = (e, defns)
+
+coreApToANF :: CoreExpr -> CoreExpr -> [CoreDefn] -> (CoreExpr, [CoreDefn])
+
+coreApToANF f (ENum n) defns
+  = (EAp f' (ENum n), defns')
+    where (f', defns') = coreExprToANF' f defns
+
+coreApToANF f (EVar v) defns
+  = (EAp f' (EVar v), defns')
+    where (f', defns') = coreExprToANF' f defns
+
+coreApToANF f a defns
+  = (EAp f' (EVar id), defns3)
+    where
+      id = newId defns
+      defns1 = (id, a') : defns
+      (f', defns2) = coreExprToANF' f defns1
+      (a', defns3) = coreExprToANF' a defns2
+
+newId :: [CoreDefn] -> Name
+newId defns = "**x" ++ (show (length defns)) ++ "**"
+
+--
+-- Saturate constructors and primitive operations
+--
+
+saturateCoreProgram :: CoreProgram -> (Name -> Int) -> CoreProgram
+saturateCoreProgram program arityOf = program
